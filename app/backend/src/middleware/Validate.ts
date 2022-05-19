c4d2a4019cc0f7d1e5bf23eeb2e9f9aaae74c576
Italo Sergio <italosergio@graduate.org>
2022-05-19 18:49:02 -0300

    fix: requirement 11 and 13, replace status 401 to 400

--- app/backend/src/middleware/Validate.ts
+++ app/backend/src/middleware/Validate.ts
@@ -4,7 +4,7 @@ import { Helper } from '../service';
 export default class Validate {
   static email(req: Request, res: Response, next: NextFunction) {
     const { email } = req.body;
-    if (!email) return res.status(401).json({ message: 'All fields must be filled' });
+    if (!email) return res.status(400).json({ message: 'All fields must be filled' });
     const regex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-z]+)$/;
     const isValidEmail = regex.test(String(email).toLowerCase());
     if (isValidEmail) return next();
@@ -13,7 +13,7 @@ export default class Validate {
 
   static async password(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
-    if (!password) return res.status(401).json({ message: 'All fields must be filled' });
+    if (!password) return res.status(400).json({ message: 'All fields must be filled' });
     const isCorrectUser = await Helper.bcrypt(email, password);
     if (isCorrectUser) return next();
     return res.status(401).json({ message: 'Incorrect email or password' });
