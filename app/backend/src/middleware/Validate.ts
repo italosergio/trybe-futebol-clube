6b440ff646bef748188dcc1b82067b2c2e015cbe
Italo Sergio <italosergio@graduate.org>
2022-05-18 23:11:45 -0300

    pass: requirement 11, build: password exists validation

--- app/backend/src/middleware/Validate.ts
+++ app/backend/src/middleware/Validate.ts
@@ -13,6 +13,7 @@ export default class Validate {
 
   static async password(req: Request, res: Response, next: NextFunction) {
     const { email, password } = req.body;
+    if (!password) return res.status(401).json({ message: 'All fields must be filled' });
     const isCorrectUser = await Helper.bcrypt(email, password);
     if (isCorrectUser) return next();
     return res.status(401).json({ message: 'Incorrect email or password' });
