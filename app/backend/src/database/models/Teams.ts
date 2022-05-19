a528bf44732bbd5cf02f2f2aa02236de8aa053bd
Italo Sergio <italosergio@graduate.org>
2022-05-19 19:45:17 -0300

    build: Teams database model

---
+++ app/backend/src/database/models/Teams.ts
@@ -0,0 +1,28 @@
+import { Model, INTEGER, STRING } from 'sequelize';
+import db from '.';
+
+class Teams extends Model {
+  id!: number;
+
+  teamName!: string;
+}
+
+Teams.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING(50),
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default Teams;
