be89c15f2c9502f88934e2764ae1571775232c2a
Italo Sergio <italosergio@graduate.org>
2022-05-18 23:10:47 -0300

    pass: requirement 11, build: email exists validation

--- app/backend/src/middleware/Validate.ts
+++ app/backend/src/middleware/Validate.ts
@@ -4,6 +4,7 @@ import { Helper } from '../service';
 export default class Validate {
   static email(req: Request, res: Response, next: NextFunction) {
     const { email } = req.body;
+    if (!email) return res.status(401).json({ message: 'All fields must be filled' });
     const regex = /^([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-z]+)$/;
     const isValidEmail = regex.test(String(email).toLowerCase());
     if (isValidEmail) return next();
