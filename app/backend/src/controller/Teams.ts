d9e8e2802633c485b47819c6b7382a3412727621
Italo Sergio <italosergio@graduate.org>
2022-05-19 19:47:45 -0300

    build: Teams controller

---
+++ app/backend/src/controller/Teams.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Response, Request } from 'express';
+import { ITeams } from '../interface';
+import Find from '../service/Find';
+
+export default class Teams {
+  private static _teams: ITeams[];
+
+  static async all(_req: Request, res: Response, _next: NextFunction): Promise<Response> {
+    this._teams = await Find.Teams();
+    return res.status(200).json(this._teams);
+  }
+}
---
+++ app/backend/src/controller/index.ts
@@ -0,0 +1,4 @@
+import Login from './Login';
+import Teams from './Teams';
+
+export { Login, Teams };
