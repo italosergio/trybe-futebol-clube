fff81e0ccb864aded8641b546f0c2d6436fc22ca
Italo Sergio <italosergio@graduate.org>
2022-05-20 02:31:29 -0300

    pass: requirements 20 and 21, GET /matches?inProgress=

--- app/backend/src/controller/Matches.ts
+++ app/backend/src/controller/Matches.ts
@@ -5,8 +5,12 @@ import { IMatch } from '../interface';
 export default class Matches {
   static _matches: IMatch[];
 
-  static async all(_req: Request, res: Response, _next: NextFunction): Promise<Response> {
+  static async all(req: Request, res: Response, _next: NextFunction): Promise<Response> {
+    const inProgress = req.query.inProgress as string;
     this._matches = await Find.Maches();
+    if (!inProgress) return res.status(200).json(this._matches);
+    this._matches = this._matches
+      .filter((match) => match.inProgress === JSON.parse(inProgress));
     return res.status(200).json(this._matches);
   }
 }
