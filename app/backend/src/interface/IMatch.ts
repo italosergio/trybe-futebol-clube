40e400f112fda99bde726171ffb6ce6002bdc911
Italo Sergio <italosergio@graduate.org>
2022-05-20 19:09:37 -0300

    refactor: team and match interfaces

--- app/backend/src/interface/IMatch.ts
+++ app/backend/src/interface/IMatch.ts
@@ -1,5 +1,4 @@
 export default interface IMatch {
-  id: number,
   homeTeam: number,
   homeTeamGoals: number,
   awayTeam: number,
--- app/backend/src/interface/ITeam.ts
+++ app/backend/src/interface/ITeam.ts
@@ -1,4 +1,3 @@
 export default interface ITeam {
-  id: number,
   teamName: string
 }
