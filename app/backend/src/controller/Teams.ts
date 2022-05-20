90f5c48278af6ca1b798b510181f530e7bc93242
Italo Sergio <italosergio@graduate.org>
2022-05-19 23:10:16 -0300

    build: Team controller

--- app/backend/src/controller/Teams.ts
+++ app/backend/src/controller/Teams.ts
@@ -9,4 +9,10 @@ export default class Teams {
     this._teams = await Find.Teams();
     return res.status(200).json(this._teams);
   }
+
+  static async byId(req: Request, res: Response, _next: NextFunction): Promise<Response> {
+    const { params: { id } } = req;
+    const team = await Find.Team(id);
+    return res.status(200).json(team);
+  }
 }
