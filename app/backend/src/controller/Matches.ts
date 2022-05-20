132afe318a6a47ac0f0a348a6fb76a5ab5651b02
Italo Sergio <italosergio@graduate.org>
2022-05-20 19:14:17 -0300

    build: create match controller

--- app/backend/src/controller/Matches.ts
+++ app/backend/src/controller/Matches.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Response, Request } from 'express';
-import { Find } from '../service';
+import { Create, Find } from '../service';
 import { IMatch } from '../interface';
 
 export default class Matches {
@@ -7,10 +7,38 @@ export default class Matches {
 
   static async all(req: Request, res: Response, _next: NextFunction): Promise<Response> {
     const inProgress = req.query.inProgress as string;
-    this._matches = await Find.Maches();
+
+    this._matches = await Find.Matches();
+
     if (!inProgress) return res.status(200).json(this._matches);
+
     this._matches = this._matches
       .filter((match) => match.inProgress === JSON.parse(inProgress));
+
     return res.status(200).json(this._matches);
+  }
+
+  static async create(req: Request, res: Response, _next: NextFunction): Promise<Response> {
+    const {
+      homeTeam,
+      awayTeam,
+      homeTeamGoals,
+      awayTeamGoals,
+      inProgress,
+    } = req.body;
+
+    const match = {
+      homeTeam,
+      awayTeam,
+      homeTeamGoals,
+      awayTeamGoals,
+      inProgress,
+    };
+
+    await Create.match(match);
+
+    const createdMatch = await Find.Match(match);
+
+    return res.status(201).json(createdMatch);
   }
 }
