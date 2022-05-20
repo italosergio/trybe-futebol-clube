b32779c5be82dc0c9b27c3914d9f8cdafa648efc
Italo Sergio <italosergio@graduate.org>
2022-05-20 01:31:56 -0300

    build: Match interface

--- app/backend/src/interface/index.ts
+++ app/backend/src/interface/index.ts
@@ -1,5 +1,6 @@
 import IUser from './IUser';
 import ITeams from './ITeams';
 import ITeam from './ITeam';
+import IMatch from './IMatch';
 
-export { IUser, ITeams, ITeam };
+export { IUser, ITeams, ITeam, IMatch };
