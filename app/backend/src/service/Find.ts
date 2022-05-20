42fc674029268163595e1056b06d9b5287f99fc2
Italo Sergio <italosergio@graduate.org>
2022-05-19 23:09:56 -0300

    build: Team service

--- app/backend/src/service/Find.ts
+++ app/backend/src/service/Find.ts
@@ -1,10 +1,12 @@
-import { IUser, ITeams } from '../interface';
-import { User, Teams } from '../model';
+import { IUser, ITeams, ITeam } from '../interface';
+import { User, Team, Teams } from '../model';
 
 export default class Find {
   static _user: IUser;
 
   static _teams: ITeams[];
+
+  static _team: ITeam;
 
   static async User(email: string) {
     const user = await User.find(email);
@@ -21,8 +23,12 @@ export default class Find {
   }
 
   static async Teams() {
-    const teams = await Teams.find();
-    this._teams = teams;
+    this._teams = await Teams.find();
     return this._teams;
+  }
+
+  static async Team(id) {
+    this._team = await Team.find(id);
+    return this._team;
   }
 }
