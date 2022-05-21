31c7584bbc74e27526c13cce6eae0cd5fd2c96f4
Italo Sergio <italosergio@graduate.org>
2022-05-20 21:33:00 -0300

    fix: interface index

--- app/backend/src/interface/index.ts
+++ app/backend/src/interface/index.ts
@@ -1,6 +1,5 @@
 import IUser from './IUser';
-import ITeams from './ITeams';
 import ITeam from './ITeam';
 import IMatch from './IMatch';
 
-export { IUser, ITeams, ITeam, IMatch };
+export { IUser, ITeam, IMatch };
