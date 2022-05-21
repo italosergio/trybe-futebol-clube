66350a033908e4df0af1b9352c23436c1ae7c42d
Italo Sergio <italosergio@graduate.org>
2022-05-21 00:55:12 -0300

    feat: implement match score on update service

--- app/backend/src/service/Update.ts
+++ app/backend/src/service/Update.ts
@@ -1,6 +1,11 @@
 import { Match } from '../model';
+
 export default class Update {
   static async matchFinish(id: string) {
-    return await Match.finish(id)
+    return Match.finish(id);
+  }
+
+  static async matchScore(id: string, homeTeamGoals: number, awayTeamGoals: number) {
+    return Match.score(id, homeTeamGoals, awayTeamGoals);
   }
 }
