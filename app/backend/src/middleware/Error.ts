e9c7d76e48cd1508e04108bb5977f9a6fa1ae326
Italo Sergio <italosergio@graduate.org>
2022-05-21 09:19:29 -0300

    feat: change status code on error middleware

--- app/backend/src/middleware/Error.ts
+++ app/backend/src/middleware/Error.ts
@@ -3,6 +3,6 @@ import { Error } from 'sequelize/types';
 
 export default class HttpError {
   static throw(err: Error, req: Request, res: Response, _next: NextFunction): Response {
-    return res.status(200).json({ message: 'Something wrong!' });
+    return res.status(500).json({ message: 'Something wrong!' });
   }
 }
