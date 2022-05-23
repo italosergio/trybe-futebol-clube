610e7b9f1636613a996e3970a0e19a0cad54375a
Italo Sergio <italosergio@graduate.org>
2022-05-23 06:59:53 -0300

    fix: undo delete routes dir

--- app/backend/src/routes/index.ts
+++
@@ -1,6 +0,0 @@
-import LoginRouter from './Login';
-import TeamsRouter from './Teams';
-import MatchesRouter from './Matches';
-import LeaderboardRouter from './Leaderboard';
-
-export { LoginRouter, TeamsRouter, MatchesRouter, LeaderboardRouter };
